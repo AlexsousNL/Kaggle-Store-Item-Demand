a4e086fbd58f465ebf311c7a59d41a287573a432
Alexis Juin <alexis.juin10@gmail.com>
2019-03-23 13:12:38 +0100

    Initialisation du projet

---
+++ main.py
@@ -0,0 +1,12 @@
+# NEW DATA CHALLENGE #
+# URL : https://www.kaggle.com/c/demand-forecasting-kernels-only/data
+
+import pandas as pd
+import numpy as np
+import fbprophet as prophet
+import sklearn
+
+train = pd.read_csv('train.csv')
+
+
+print('End of Program')
